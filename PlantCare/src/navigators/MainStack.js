82791b0c8910c838ec2ecf9878cf3b982f8837d0
DimasAditya2 <esadimasadityaa@gmail.com>
2024-07-14 12:00:34 +0700

    Revert "feat: Notification, Faq, EditProfile screen, modify profile screen"
    This reverts commit 5e70bc7a0b8d4d49e91a9651385226a56653e757.

--- PlantCare/src/navigators/MainStack.js
+++ PlantCare/src/navigators/MainStack.js
@@ -1,25 +1,20 @@
-import React, { useContext } from "react";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Ionicons } from "@expo/vector-icons";
+import React, { useContext } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Ionicons } from '@expo/vector-icons';
 
-import MyPlantScreen from "../screens/MyPlantScreen";
-import PlantInfoScreen from "../screens/PlantInfoScreen";
-import AddPlantScreen from "../screens/AddPlantScreen";
-import AddPlantFormScreen from "../screens/AddPlantFormScreen";
-import ProfileScreen from "../screens/ProfileScreen";
-import RegisterScreen from "../screens/RegisterScreen";
-import LoginScreen from "../screens/LoginScreen";
-import PreferenceScreen from "../screens/PreferenceScreen";
-import SplashScreen from "../screens/SplashScreen";
+import MyPlantScreen from '../screens/MyPlantScreen';
+import PlantInfoScreen from '../screens/PlantInfoScreen';
+import AddPlantScreen from '../screens/AddPlantScreen';
+import AddPlantFormScreen from '../screens/AddPlantFormScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import LoginScreen from '../screens/LoginScreen';
+import PreferenceScreen from '../screens/PreferenceScreen';
+import SplashScreen from '../screens/SplashScreen';
 
-import { authContext } from "../contexts/authContext";
-import FaqScreen from "../screens/FaqScreen";
-import { Text, TouchableOpacity } from "react-native";
-import NotificationSetting from "../screens/NotificationSetting";
-import { View } from "react-native";
-import EditProfileScreen from "../screens/EditProfileScreen";
+import { authContext } from '../contexts/authContext';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -31,31 +26,27 @@ function TabNavigator() {
         tabBarIcon: ({ color, size }) => {
           let iconName;
 
-          if (route.name === "MyPlant") {
-            iconName = "leaf";
-          } else if (route.name === "AddPlant") {
-            iconName = "add-circle";
-          } else if (route.name === "Profile") {
-            iconName = "person";
+          if (route.name === 'MyPlant') {
+            iconName = 'leaf';
+          } else if (route.name === 'AddPlant') {
+            iconName = 'add-circle';
+          } else if (route.name === 'Profile') {
+            iconName = 'person';
           }
 
           return <Ionicons name={iconName} size={size} color={color} />;
         },
-        tabBarActiveTintColor: "tomato",
-        tabBarInactiveTintColor: "gray",
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
         tabBarShowLabel: false,
         tabBarStyle: {
-          display: "flex",
+          display: 'flex',
         },
       })}
     >
       <Tab.Screen name="MyPlant" component={MyPlantScreen} />
       <Tab.Screen name="AddPlant" component={AddPlantScreen} />
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{ headerShown: false }}
-      />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
 }
@@ -105,67 +96,6 @@ export default function MainStack() {
               name="AddPlantForm"
               component={AddPlantFormScreen}
               options={{ headerShown: true }}
-            />
-            <Stack.Screen
-              name="Faq"
-              component={FaqScreen}
-              options={({ navigation }) => ({
-                headerShown: true,
-                title: "",
-                headerLeft: () => (
-                  <TouchableOpacity onPress={() => navigation.goBack()}>
-                    <View
-                      style={{
-                        flexDirection: "row",
-                        alignItems: "center",
-                        gap: 4,
-                      }}
-                    >
-                      <Ionicons name="arrow-back" size={24} color="black" />
-                      <Text style={{ fontSize: 18, fontWeight: "bold" }}>
-                        Profile
-                      </Text>
-                    </View>
-                  </TouchableOpacity>
-                ),
-              })}
-            />
-            <Stack.Screen
-              name="EditProfile"
-              component={EditProfileScreen}
-              options={({ navigation }) => ({
-                headerShown: true,
-                title: "",
-                headerLeft: () => (
-                  <TouchableOpacity onPress={() => navigation.goBack()}>
-                    <View
-                      style={{
-                        flexDirection: "row",
-                        alignItems: "center",
-                        gap: 4,
-                      }}
-                    >
-                      <Ionicons name="arrow-back" size={24} color="black" />
-                      <Text style={{ fontSize: 18, fontWeight: "bold" }}>
-                        Profile
-                      </Text>
-                    </View>
-                  </TouchableOpacity>
-                ),
-              })}
-            />
-            <Stack.Screen
-              name="notification"
-              component={NotificationSetting}
-              options={({ navigation }) => ({
-                headerShown: true,
-                title: "Notification Setting",
-                headerLeft: () => (
-                  <TouchableOpacity onPress={() => navigation.goBack()}>
-                    <Ionicons name="arrow-back" size={24} color="black" />
-                  </TouchableOpacity>
-                ),
-              })}
             />
           </>
         )}
--- PlantCare/src/screens/ProfileScreen.js
+++ PlantCare/src/screens/ProfileScreen.js
@@ -1,135 +1,44 @@
-import React, { useContext, useState } from "react";
-import {
-  View,
-  Text,
-  Image,
-  StyleSheet,
-  TouchableOpacity,
-  ScrollView,
-  SafeAreaView,
-  Switch,
-  Dimensions,
-} from "react-native";
-
-import Ionicons from "react-native-vector-icons/Ionicons";
-import { useNavigation } from "@react-navigation/native";
-import { authContext } from "../contexts/authContext";
-
-// mengambil lebar layar
-const { width } = Dimensions.get("window");
+import React, { useContext } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { authContext } from '../contexts/authContext';
 
 const ProfileScreen = () => {
   const navigation = useNavigation();
   const { logout } = useContext(authContext);
-  const [notificationsEnabled, setNotificationsEnabled] = useState(false);
+
   const handleLogout = () => {
     logout();
-    navigation.navigate("Login");
+    navigation.navigate('Login');
   };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <SafeAreaView>
-        <View style={styles.header}>
-          <View style={styles.coverContainer}>
-            <Image
-              source={{
-                uri: "https://static.vecteezy.com/system/resources/thumbnails/006/224/670/small/go-green-concept-banner-with-lush-green-foliage-illustration-vector.jpg", // Ganti dengan URL gambar sampul yang diinginkan
-              }}
-              style={styles.coverImage}
-            />
-            <Image
-              source={{
-                uri: "https://cdn.idntimes.com/content-images/post/20240226/remu19971203-423414763-1419413852325257-8160633298947598858-n-a83f7868577039e47e2344a96107eea1.jpg",
-              }}
-              style={styles.imageProfile}
-            />
-          </View>
-          <Text style={styles.profileName}>remu suzumori</Text>
-          <Text style={styles.profileSubText}>suzumori@gmail.com</Text>
+      <Image
+        source={{ uri: 'https://via.placeholder.com/100' }}
+        style={styles.profileImage}
+      />
+      <Text style={styles.name}>User Name</Text>
+      <Text style={styles.email}>user@mail.com</Text>
+      <View style={styles.infoContainer}>
+        <View style={styles.infoBox}>
+          <Text style={styles.label}>Level</Text>
+          <Text style={styles.value}>Beginner</Text>
         </View>
-
-        <View style={styles.WrapMenu}>
-          <View style={styles.userInfo}>
-            <View style={styles.userItem}>
-              <Text style={styles.itemLabel}>Level</Text>
-              <Text style={styles.itemValue}>Beginner</Text>
-            </View>
-
-            <View style={styles.userItem}>
-              <Text style={styles.itemLabel}>Plant</Text>
-              <Text style={styles.itemValue}>10</Text>
-            </View>
-          </View>
-          <View style={styles.menuItem}>
-            <View style={styles.wrapText}>
-              <Ionicons
-                name="information-circle-outline"
-                size={20}
-                color="#333"
-              />
-              <Text style={styles.menuItemText}>Account Information</Text>
-            </View>
-            <Ionicons name="chevron-forward-outline" size={20} color="#333" />
-          </View>
-          <TouchableOpacity onPress={() => navigation.navigate("EditProfile")}>
-            <View style={styles.menuItem}>
-              <View style={styles.wrapText}>
-                <Ionicons name="create-outline" size={20} color="#333" />
-                <Text style={styles.menuItemText}>Edit Profile</Text>
-              </View>
-              <Ionicons name="chevron-forward-outline" size={20} color="#333" />
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate("notification")}>
-            <View style={styles.menuItem}>
-              <View style={styles.wrapText}>
-                <Ionicons name="notifications-outline" size={20} color="#333" />
-                <Text style={styles.menuItemText}>Notifications</Text>
-              </View>
-              <Switch
-                value={notificationsEnabled}
-                onValueChange={setNotificationsEnabled}
-              />
-            </View>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.navigate("Faq")}>
-            <View style={styles.menuItem}>
-              <View style={styles.wrapText}>
-                <Ionicons name="help-circle-outline" size={20} color="#333" />
-                <Text style={styles.menuItemText}>FAQ</Text>
-              </View>
-              <Ionicons name="chevron-forward-outline" size={20} color="#333" />
-            </View>
-          </TouchableOpacity>
+        <View style={styles.infoBox}>
+          <Text style={styles.label}>Plants</Text>
+          <Text style={styles.value}>7</Text>
         </View>
-        <View
-          style={{
-            flex: 1,
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <TouchableOpacity onPress={handleLogout}>
-            <View
-              style={{
-                // backgroundColor: "red",
-                flexDirection: "row",
-                alignItems: "center",
-                gap: 5,
-              }}
-            >
-              <Ionicons name="log-out-outline" size={30} color="#ff0000" />
-              <Text
-                style={{ fontSize: "20", fontWeight: "400", color: "black" }}
-              >
-                Logout
-              </Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
+      </View>
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>Notifications</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonText}>FAQ</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+        <Text style={styles.logoutButtonText}>Logout</Text>
+      </TouchableOpacity>
     </ScrollView>
   );
 };
@@ -137,91 +46,99 @@ const ProfileScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    backgroundColor: "#FFF5F5",
-    alignItems: "center",
+    padding: 16,
+    backgroundColor: '#f8f8f8',
+    alignItems: 'center',
   },
-  header: {
-    alignItems: "center",
-    marginVertical: 20,
-    marginTop: 10,
-  },
-  coverContainer: {
-    alignItems: "center",
-    position: "relative",
-  },
-  coverImage: {
-    // backgroundColor: "pink",
-    width: width,
-    height: 150,
-    position: "absolute",
-    top: 0,
-    zIndex: -1,
-  },
-  imageProfile: {
+  profileImage: {
     width: 100,
     height: 100,
     borderRadius: 50,
-    marginBottom: 10,
-    marginTop: 75,
+    marginBottom: 16,
   },
-  profileName: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "#333",
+  name: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 4,
   },
-  profileSubText: {
-    fontSize: 14,
-    color: "#666",
+  email: {
+    fontSize: 18,
+    color: '#666',
+    marginBottom: 16,
   },
-  userInfo: {
-    // backgroundColor: "blue",
-    flexDirection: "row",
+  infoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    marginBottom: 24,
   },
-  userItem: {
-    // flexDirection: 'row',
-    marginBottom: 10,
-    borderRadius: 10,
-    width: "45%",
-    margin: "auto",
-    backgroundColor: "white",
-    padding: 20,
-    shadowColor: "#171717",
-    shadowOffset: { width: -1, height: 4 },
+  infoBox: {
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
     shadowOpacity: 0.2,
-    shadowRadius: 3,
+    shadowRadius: 1.41,
+    elevation: 2,
+    width: '45%',
   },
-  itemLabel: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  WrapMenu: {
-    flex: 1,
-    gap: 3,
-  },
-  menuItem: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "95%",
-    margin: "auto",
-    padding: 15,
-    marginVertical: 5,
-    backgroundColor: "#fff",
-    borderRadius: 10,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 5,
-    elevation: 3,
-  },
-  menuItemText: {
+  label: {
     fontSize: 16,
-    color: "#333",
+    color: '#888',
+    marginBottom: 4,
   },
-  wrapText: {
-    // backgroundColor: "pink",
-    flexDirection: "row",
-    gap: 10,
+  value: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+  button: {
+    width: '100%',
+    padding: 16,
+    backgroundColor: '#4caf50',
+    borderRadius: 8,
+    alignItems: 'center',
+    marginBottom: 16,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  logoutButton: {
+    width: '100%',
+    padding: 16,
+    backgroundColor: '#F44336',
+    borderRadius: 8,
+    alignItems: 'center',
+    marginBottom: 16,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+  },
+  logoutButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff',
   },
 });
 
