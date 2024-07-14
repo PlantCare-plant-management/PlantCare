0b45fb946862b32728b74b272d92eb11757c64aa
nrahmatk <nrahmatkhairi@gmail.com>
2024-07-14 09:42:33 +0700

    server client

--- PlantCare/src/navigators/MainStack.js
+++ PlantCare/src/navigators/MainStack.js
@@ -1,20 +1,21 @@
-import React, { useContext } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Ionicons } from '@expo/vector-icons';
+import React, { useContext } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Ionicons } from "@expo/vector-icons";
 
-import MyPlantScreen from '../screens/MyPlantScreen';
-import PlantInfoScreen from '../screens/PlantInfoScreen';
-import AddPlantScreen from '../screens/AddPlantScreen';
-import AddPlantFormScreen from '../screens/AddPlantFormScreen';
-import ProfileScreen from '../screens/ProfileScreen';
-import RegisterScreen from '../screens/RegisterScreen';
-import LoginScreen from '../screens/LoginScreen';
-import PreferenceScreen from '../screens/PreferenceScreen';
-import SplashScreen from '../screens/SplashScreen';
+import MyPlantScreen from "../screens/MyPlantScreen";
+import PlantInfoScreen from "../screens/PlantInfoScreen";
+import AddPlantScreen from "../screens/AddPlantScreen";
+import AddPlantFormScreen from "../screens/AddPlantFormScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import RegisterScreen from "../screens/RegisterScreen";
+import LoginScreen from "../screens/LoginScreen";
+import PreferenceScreen from "../screens/PreferenceScreen";
+import SplashScreen from "../screens/SplashScreen";
 
-import { authContext } from '../contexts/authContext';
+import { authContext } from "../contexts/authContext";
+import AddRoomPlantsScreen from "../screens/AddRoomPlantsScreen";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -26,21 +27,21 @@ function TabNavigator() {
         tabBarIcon: ({ color, size }) => {
           let iconName;
 
-          if (route.name === 'MyPlant') {
-            iconName = 'leaf';
-          } else if (route.name === 'AddPlant') {
-            iconName = 'add-circle';
-          } else if (route.name === 'Profile') {
-            iconName = 'person';
+          if (route.name === "MyPlant") {
+            iconName = "leaf";
+          } else if (route.name === "AddPlant") {
+            iconName = "add-circle";
+          } else if (route.name === "Profile") {
+            iconName = "person";
           }
 
           return <Ionicons name={iconName} size={size} color={color} />;
         },
-        tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'gray',
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
         tabBarShowLabel: false,
         tabBarStyle: {
-          display: 'flex',
+          display: "flex",
         },
       })}
     >
@@ -95,6 +96,11 @@ export default function MainStack() {
             <Stack.Screen
               name="AddPlantForm"
               component={AddPlantFormScreen}
+              options={{ headerShown: true }}
+            />
+            <Stack.Screen
+              name="AddRoomPlant"
+              component={AddRoomPlantsScreen}
               options={{ headerShown: true }}
             />
           </>
--- PlantCare/src/screens/MyPlantScreen.js
+++ PlantCare/src/screens/MyPlantScreen.js
@@ -1,43 +1,70 @@
-// MyPlantScreen.js
-import React, { useState } from 'react';
-import { View, Text, FlatList, TouchableOpacity, StyleSheet, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const MyPlantScreen = () => {
   const navigation = useNavigation();
+  const [myPlants, setMyPlants] = useState([]);
 
-  const plants = [
-    { id: '1', name: 'Lavender', date: '01/07/24', location: 'Bedroom' },
-    { id: '2', name: 'Rose', date: '15/06/24', location: 'Kitchen' },
-    { id: '3', name: 'Tulip', date: '20/06/24', location: 'Living Room' },
-  ];
+  const fetchData = async () => {
+    try {
+      const response = await fetch(process.env.EXPO_PUBLIC_API_URL + "/plants", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        cache: "no-store",
+      });
 
-  const [filter, setFilter] = useState('All plants');
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
 
-  const filteredPlants = filter === 'All plants' ? plants : plants.filter(plant => plant.location === filter);
+      const data = await response.json();
+
+      setMyPlant(data)
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <View style={styles.container}>
       <Text style={styles.date}>Wednesday, 10 July 2024</Text>
       <View style={styles.filterContainer}>
-        <TouchableOpacity style={[styles.filterButton, filter === 'All plants' && styles.selectedFilter]} onPress={() => setFilter('All plants')}>
+        {/* <TouchableOpacity
+          style={[
+            styles.filterButton,
+            filter === "All plants" && styles.selectedFilter,
+          ]}
+          onPress={() => setFilter("All plants")}
+        >
           <Text style={styles.filterText}>All plants</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.filterButton, filter === 'Bedroom' && styles.selectedFilter]} onPress={() => setFilter('Bedroom')}>
-          <Text style={styles.filterText}>Bedroom</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.filterButton, filter === 'Kitchen' && styles.selectedFilter]} onPress={() => setFilter('Kitchen')}>
-          <Text style={styles.filterText}>Kitchen</Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
       </View>
       <FlatList
-        data={filteredPlants}
+        data={myPlants}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <TouchableOpacity style={styles.plantItem} onPress={() => navigation.navigate('PlantInfo', { plantId: item.id })}>
+          <TouchableOpacity
+            style={styles.plantItem}
+            onPress={() => navigation.navigate("PlantInfo", { plant: item })}
+          >
             <View style={styles.plantInfo}>
               <Text style={styles.plantName}>{item.name}</Text>
-              <Text style={styles.plantDate}>Date planted: {item.date}</Text>
+              <Text style={styles.plantDate}>
+                Latin name: {item.latin_name}
+              </Text>
             </View>
             <View style={styles.plantPhotoContainer}>
               <Text style={styles.plantPhotoText}>Foto Tanaman</Text>
@@ -45,7 +72,10 @@ const MyPlantScreen = () => {
           </TouchableOpacity>
         )}
       />
-      <TouchableOpacity style={styles.addButton} onPress={() => navigation.navigate('AddPlant')}>
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => navigation.navigate("AddPlant")}
+      >
         <Text style={styles.addButtonText}>Add a new plant</Text>
       </TouchableOpacity>
     </View>
@@ -56,37 +86,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   date: {
     fontSize: 16,
-    color: '#888',
+    color: "#888",
     marginBottom: 16,
   },
   filterContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginBottom: 16,
   },
   filterButton: {
     padding: 8,
     marginRight: 8,
-    backgroundColor: '#e0e0e0',
+    backgroundColor: "#e0e0e0",
     borderRadius: 8,
   },
   selectedFilter: {
-    backgroundColor: '#a0e0a0',
+    backgroundColor: "#a0e0a0",
   },
   filterText: {
     fontSize: 16,
-    color: '#333',
+    color: "#333",
   },
   plantItem: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     padding: 16,
     marginBottom: 16,
-    backgroundColor: '#f9f9f9',
+    backgroundColor: "#f9f9f9",
     borderRadius: 8,
   },
   plantInfo: {
@@ -94,37 +124,37 @@ const styles = StyleSheet.create({
   },
   plantName: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   plantDate: {
     fontSize: 14,
-    color: '#666',
+    color: "#666",
   },
   plantPhotoContainer: {
     width: 80,
     height: 80,
-    backgroundColor: '#d0d0d0',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#d0d0d0",
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 8,
   },
   plantPhotoText: {
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
   },
   addButton: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 16,
     right: 16,
     padding: 16,
-    backgroundColor: '#4caf50',
+    backgroundColor: "#4caf50",
     borderRadius: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   addButtonText: {
-    color: '#fff',
-    fontWeight: 'bold',
+    color: "#fff",
+    fontWeight: "bold",
   },
 });
 
