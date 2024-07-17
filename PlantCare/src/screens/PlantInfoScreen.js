bf00b92f167763f6f04ec8003d0576f8e197fc72
Muhammad Ridho Amrullah <ridhoamrullah99@gmail.com>
2024-07-17 21:30:13 +0700

    Feat: PlantInfo fix

--- PlantCare/src/screens/PlantInfoScreen.js
+++ PlantCare/src/screens/PlantInfoScreen.js
@@ -1,77 +1,157 @@
-import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
-import { useRoute } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { CheckBox } from 'react-native-elements';
+import React, { useState } from "react";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { CheckBox } from "react-native-elements";
 
 const PlantInfoScreen = () => {
   const route = useRoute();
   const { plant } = route.params;
-  // console.log(plant.plants)
-  const [actions, setActions] = useState(plant.actions.filter(action => action.show));
+  console.log(plant.plants, "ini plants");
+  const recommendations = plant.plants.recommendation || [];
+  const mainCareTasks = plant.plants.main_care || [];
+  console.log(recommendations, " ini recom");
+  console.log(mainCareTasks, " ini main");
+  const [actions, setActions] = useState(
+    plant.actions.filter((action) => action.show)
+  );
 
-  const toggleTask = async(taskId) =>{
+  const toggleTask = async (taskId) => {
     try {
       const updatedActions = actions.map((action) =>
-        action.id === taskId ? { ...action, status: !action.status, update:new Date() } : action
+        action.id === taskId
+          ? { ...action, status: !action.status, update: new Date() }
+          : action
       );
       setActions(updatedActions);
 
       const plantId = plant._id;
-      const response = await fetch(process.env.EXPO_PUBLIC_API_URL + "/myplants/update", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          plantId,
-          actions: updatedActions 
-        }),
-      });
+      const response = await fetch(
+        process.env.EXPO_PUBLIC_API_URL + "/myplants/update",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            plantId,
+            actions: updatedActions,
+          }),
+        }
+      );
       const result = await response.json();
       console.log(result);
     } catch (error) {
       console.log(error);
     }
   };
+
+  const careIcons = {
+    Watering: "watering-can",
+    Light: "weather-sunny",
+    Lighting: "weather-sunny",
+    Soil: "spa",
+    Fertilizing: "leaf",
+    Pruning: "content-cut",
+    Repotting: "flower-tulip",
+    "Monitor plant health": "monitor",
+  };
+
+  const careColors = {
+    Watering: "#AED581",
+    Light: "#FFD54F",
+    Lighting: "#FFD54F",
+    Soil: "#8D6E63",
+    Fertilizing: "#8BC34A",
+    Pruning: "#FFC107",
+    Repotting: "#F48FB1",
+    "Monitor plant health": "#2196F3",
+  };
   // console.log(new Date(actions[1].update)- new Date())
   return (
-    <ScrollView contentContainerStyle={styles.scrollViewContainer}>
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <View style={styles.textContainer}>
-            <Text style={styles.title}>{plant.name}</Text>
-            <Text style={styles.description}>
-              {plant.plants.description}
-              <Text style={styles.readMore}> Read more</Text>
+    <ScrollView style={styles.mainContainer}>
+      <View style={styles.contentContainer}>
+        <View style={styles.titleContainer}>
+          {/* Awal Plant Title */}
+          <Text style={styles.namaTanaman}>{plant.plants.name}</Text>
+          {/* Akhir Plant Title */}
+          {/* Awal Plant Image */}
+          <Image
+            source={{
+              uri: plant.plants.imgUrl || "https://via.placeholder.com/100",
+            }}
+            style={styles.gambarTanaman}
+          />
+        </View>
+        {/* Akhir Plant Image */}
+
+        {/* Awal Plant Description */}
+        <Text style={styles.deskripsiTanaman}>{plant.plants.description}</Text>
+        {/* Akhir Plant Description */}
+
+        {/* Awal Difficulty and Harvest */}
+        <View style={styles.difficulty}>
+          <View style={styles.setelahDifficulty}>
+            <Text style={styles.textDifficulty}>Difficulty</Text>
+            <Text style={styles.levelDifficulty}>
+              {plant.plants.difficulty}
             </Text>
           </View>
-          <Image source={{ uri: `${plant.plants.imgUrl}` }} style={styles.image} />
-        </View>
-        <View style={styles.infoContainer}>
-          <View style={styles.infoBox}>
-            <Text style={styles.infoLabel}>Difficulty</Text>
-            <Text style={styles.infoValue}>Intermediate</Text>
-          </View>
-          <View style={styles.infoBox}>
-            <Text style={styles.infoLabel}>Ready to harvest</Text>
-            <Text style={styles.infoValue}>False</Text>
+
+          <View>
+            <Text style={styles.readyToHarvest}>Ready To Harvest</Text>
+            <Text style={styles.waktuHarvest}>{plant.plants.harvest}</Text>
           </View>
         </View>
-        <Text style={styles.sectionTitle}>Recommendations</Text>
-        {plant.plants.recommendation.map((rec, index) => (
-          <View key={index} style={styles.recommendationBox}>
-            <Text style={styles.recommendationText}>{rec}</Text>
-          </View>
-        ))}
-        <Text style={styles.sectionTitle}>Main care</Text>
-        {plant.plants.main_care.map((care, index) => (
-          <View key={index} style={styles.careBox}>
-            <Icon name={getActionIcon(care.task)} size={40} color="#333" style={styles.careIcon} />
-            <Text style={styles.careText}>{care.task}</Text>
-          </View>
-        ))}
-        <Text style={styles.sectionTitle}>Task Today</Text>
+        {/* Akhir Difficulty and Harvest */}
+
+        {/* Awal Recommendations */}
+        <View style={styles.reccomendation}>
+          <Text style={styles.textRecommendation}>Recommendations</Text>
+
+          <ScrollView horizontal={true} style={styles.scrollViewCardRecom}>
+            {recommendations.map((rec, index) => {
+              const [task, description] = rec.split(": ");
+              return (
+                <View key={index} style={styles.cardRecommendation}>
+                  <View style={styles.iconContainer}>
+                    <Icon
+                      name={careIcons[task]}
+                      size={30}
+                      color={careColors[task]}
+                    />
+                  </View>
+                  <View style={styles.recom}>
+                    <Text style={styles.textTaskRecom}>{task}</Text>
+                    <Text style={styles.descriptionTask}>{description}</Text>
+                  </View>
+                </View>
+              );
+            })}
+          </ScrollView>
+        </View>
+        {/* Akhir Recommendations */}
+
+        {/* Awal Main Care */}
+        <View style={styles.mainCare}>
+          <Text style={styles.textMainCare}>Main Care</Text>
+          <ScrollView horizontal={true} style={styles.scrollViewCardMainCare}>
+            {plant.plants.main_care.map((care, index) => (
+              <View key={index} style={styles.cardMainCare}>
+                <Icon
+                  name={getActionIcon(care.task)}
+                  size={40}
+                  color="#333"
+                  style={styles.careIcon}
+                />
+                <Text style={styles.careText}>{care.task}</Text>
+              </View>
+            ))}
+          </ScrollView>
+        </View>
+        {/* Akhir Main Care */}
+
+        <Text style={styles.sectionTitleTask}>Task Today</Text>
         <View style={styles.taskTodayContainer}>
           {actions.map((task) => (
             <View key={task.id} style={styles.taskBox}>
@@ -81,8 +161,11 @@ const PlantInfoScreen = () => {
                 disabled={task.status}
                 containerStyle={styles.taskCheckbox}
               />
-              <Icon name={getActionIcon(task.name)} size={30} color={task.status ? '#AED581' : '#333'} />
-              <Text>{task.name}</Text>
+              <Icon
+                name={getActionIcon(task.name)}
+                size={30}
+                color={task.status ? "#AED581" : "#333"}
+              />
             </View>
           ))}
         </View>
@@ -93,31 +176,37 @@ const PlantInfoScreen = () => {
 
 const getActionIcon = (actionName) => {
   switch (actionName.toLowerCase()) {
-    case 'watering':
-      return 'watering-can';
-    case 'lighting':
-      return 'weather-sunny';
-    case 'fertilizing':
-      return 'sprout';
-    case 'monitor plant health':
-      return 'leaf';
+    case "watering":
+      return "watering-can";
+    case "lighting":
+      return "weather-sunny";
+    case "fertilizing":
+      return "sprout";
+    case "monitor plant health":
+      return "heart-pulse";
+    case "pruning":
+      return "content-cut";
+    case "staking":
+      return "content-cut";
+    case "harvesting":
+      return "cart-arrow-down";
     default:
-      return 'help-circle-outline';
+      return "help-circle-outline";
   }
 };
 
 const styles = StyleSheet.create({
   scrollViewContainer: {
     padding: 16,
-    backgroundColor: '#E8F5E9',
+    backgroundColor: "#E8F5E9",
   },
   container: {
     flex: 1,
   },
   header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 16,
   },
   textContainer: {
@@ -131,103 +220,292 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
   },
   description: {
     fontSize: 16,
-    color: '#555',
+    color: "#555",
   },
   readMore: {
-    color: '#4CAF50',
+    color: "#4CAF50",
   },
   infoContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginBottom: 16,
   },
   infoBox: {
     flex: 1,
     padding: 16,
     borderRadius: 8,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     marginHorizontal: 4,
-    alignItems: 'center',
+    alignItems: "center",
   },
   infoLabel: {
     fontSize: 14,
-    color: '#888',
+    color: "#888",
     marginBottom: 4,
   },
   infoValue: {
     fontSize: 16,
-    fontWeight: 'bold',
-    color: '#333',
+    fontWeight: "bold",
+    color: "#333",
   },
   sectionTitle: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 8,
-    color: '#333',
+    color: "#333",
   },
   recommendationsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginBottom: 16,
   },
   recommendationBox: {
     flex: 1,
     padding: 16,
     borderRadius: 8,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     marginHorizontal: 4,
-    alignItems: 'center',
+    alignItems: "center",
   },
   recommendationText: {
     fontSize: 14,
-    color: '#333',
+    color: "#333",
   },
   careContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginBottom: 16,
-  },
-  careBox: {
-    flex: 1,
-    padding: 16,
-    borderRadius: 8,
-    backgroundColor: '#fff',
-    marginHorizontal: 4,
-    alignItems: 'center',
   },
   careIcon: {
     marginBottom: 8,
   },
   careText: {
     fontSize: 14,
-    fontWeight: 'bold',
-    color: '#333',
+    fontWeight: "bold",
+    color: "#333",
   },
   taskTodayContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    marginTop: 16,
+    flexDirection: "row",
+    flexWrap: "wrap",
   },
   taskBox: {
-    width: '30%',
-    margin: '1.66%',
+    width: "30%",
+    margin: "1.66%",
     padding: 16,
     borderRadius: 8,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'center',
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
   },
   taskCheckbox: {
     marginRight: 8,
     padding: 0,
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
     borderWidth: 0,
+  },
+  sectionTitle: {
+    marginLeft: 30,
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 8,
+    color: "#333",
+  },
+  sectionTitleTask: {
+    marginLeft: 30,
+    fontSize: 14,
+    fontWeight: "bold",
+    marginBottom: 8,
+    color: "#333",
+  },
+  recommendationsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 16,
+  },
+  recommendationBox: {
+    flex: 1,
+    padding: 16,
+    borderRadius: 8,
+    backgroundColor: "#fff",
+    marginHorizontal: 4,
+    alignItems: "center",
+  },
+  recommendationIcon: {
+    marginBottom: 8,
+  },
+  recommendationText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  recommendationSubText: {
+    fontSize: 12,
+    color: "#888",
+  },
+  careBox: {
+    flex: 1,
+    padding: 16,
+    borderRadius: 8,
+    backgroundColor: "#fff",
+    marginHorizontal: 4,
+    alignItems: "center",
+  },
+  taskCheckbox: {
+    marginRight: 8,
+    padding: 0,
+    backgroundColor: "transparent",
+    borderWidth: 0,
+  },
+  mainContainer: {
+    flex: 1,
+    backgroundColor: "green",
+  },
+  contentContainer: {
+    backgroundColor: "white",
+    flex: 3,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    marginTop: 150,
+    minHeight: "100%",
+  },
+  titleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  namaTanaman: {
+    fontWeight: "bold",
+    marginTop: 30,
+    fontSize: 25,
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  gambarTanaman: {
+    marginRight: 30,
+    marginTop: 20,
+    height: 100,
+    width: 100,
+  },
+  deskripsiTanaman: {
+    marginLeft: 30,
+    marginTop: 10,
+    marginRight: 30,
+  },
+  difficulty: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginRight: 30,
+    marginLeft: 30,
+  },
+  setelahDifficulty: {
+    marginTop: 30,
+  },
+  textDifficulty: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  levelDifficulty: {
+    marginTop: 5,
+  },
+  readyToHarvest: {
+    marginTop: 30,
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  waktuHarvest: {
+    marginTop: 5,
+  },
+  reccomendation: {
+    marginLeft: 30,
+    marginTop: 20,
+  },
+  textRecommendation: {
+    fontWeight: "bold",
+  },
+  scrollViewCardRecom: {
+    flexDirection: "row",
+  },
+  mainCare: {
+    marginLeft: 30,
+    marginTop: 20,
+  },
+  textMainCare: {
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  scrollViewCardMainCare: {
+    flexDirection: "row",
+  },
+  cardMainCare: {
+    backgroundColor: "#fff",
+    borderColor: "#ddd",
+    borderWidth: 1,
+    height: 150,
+    width: 120,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    marginRight: 10,
+    marginTop: 5,
+    padding: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 2,
+    marginBottom: 20,
+  },
+  setelahCardMainCare: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textTask: {
+    marginTop: 10,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  cardRecommendation: {
+    backgroundColor: "white",
+    borderColor: "#ddd",
+    borderWidth: 1,
+    height: 180,
+    width: 180,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    marginRight: 10,
+    marginTop: 5,
+    padding: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+  iconContainer: {
+    backgroundColor: "#f2f2f2",
+    borderRadius: 20,
+    padding: 10,
+    marginBottom: 5,
+  },
+  recom: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textTaskRecom: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 14,
+    color: "#333",
+  },
+  descriptionTask: {
+    textAlign: "center",
+    fontSize: 12,
+    color: "#777",
   },
 });
 
